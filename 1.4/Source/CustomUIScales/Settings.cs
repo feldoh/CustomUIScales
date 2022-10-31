05afd17215c4f19c422dee28ac12fd1a2f854af7
Dexter Lowe <feldoh10@gmail.com>
2022-10-31 03:05:02 +0000

    Initial commit

---
+++ 1.4/Source/CustomUIScales/Settings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace CustomUIScales
+{
+	public class Settings : ModSettings
+	{
+		//Use Mod.settings.setting to refer to this setting.
+		public float UIScale;
+		private string _uiScaleBuffer;
+		public bool ApplyCustomUIScale;
+
+		public void DoWindowContents(Rect wrect)
+		{
+			var options = new Listing_Standard();
+			options.Begin(wrect);
+
+			options.TextFieldNumericLabeled("UI Scale", ref UIScale, ref _uiScaleBuffer, -0.999f, float.MaxValue);
+			options.Gap();
+			options.CheckboxLabeled("Apply custom UI Scale", ref ApplyCustomUIScale);
+			options.End();
+		}
+
+		public override void ExposeData()
+		{
+			Scribe_Values.Look(ref UIScale, "customUIScale");
+			Scribe_Values.Look(ref ApplyCustomUIScale, "applyCustomUIScale");
+
+
+			if (Scribe.mode == LoadSaveMode.LoadingVars || Scribe.mode == LoadSaveMode.ResolvingCrossRefs ||
+			    !ApplyCustomUIScale || UIScale == 0 ||
+			    !(Math.Abs(Prefs.UIScale - (double)UIScale) > 0.01)) return;
+			
+			if (Math.Abs(UIScale - 1.0) > 0.01 &&
+			    !ResolutionUtility.UIScaleSafeWithResolution(UIScale, Screen.width, Screen.height))
+			{
+				Messages.Message("MessageScreenResTooSmallForUIScale".Translate(), MessageTypeDefOf.RejectInput, false);
+			}
+			else
+			{
+				ResolutionUtility.SafeSetUIScale(UIScale);
+				Prefs.Save();
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Custom UIScales
+
+Rimworld Mod to allow any custom UI Scale you like, over the 9 provided in the base game.
+Simply open up the mod settings, type in a custom size and away you go.
+
+Note that if you manage to do something weird like make the UI so small you can't see it, simply deleting the config file from:
+`%appdata%\..\LocalLow\Ludeon Studios\RimWorld by Ludeon Studios\Config` will reset it.
+
+## Attribution
+* Ludeon for such a great game with excellent mod support.
+* This mod was made for [MrSamuelStreamer](https://www.youtube.com/c/MrSamStreamer)
